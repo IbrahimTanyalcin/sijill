ff4a9692d90874a99ad05d06546fc17a8df40261
Ibrahim Tanyalcin <8837895+IbrahimTanyalcin@users.noreply.github.com>
2025-10-03 14:32:35 +0200

    try adding JOSS paper

---
+++ static/md/papers/JOSS/xpress-bio.joss.pdf.js
@@ -0,0 +1,9 @@
+!function(){
+    ch`
+    0> container:${document.querySelector(".text-container-1")}
+    -> self:${ch.iframe}
+    satr src ${"https://ibrahimtanyalcin.github.io/sijill/papers/JOSS/xpress-bio-paper.pdf"}
+    style ${[["width", "100%"], ["height", "100dvh"]]}
+    +-< ${({values:v}) => v.container}
+    `
+}()
